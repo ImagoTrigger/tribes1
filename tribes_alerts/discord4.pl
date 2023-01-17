3c0206b572302ab58934916381a85f07d8d8a406
Allegiance Zone pusher <imagotrigger@gmail.com>
2023-01-17 16:59:18 -0600

    delete previous message

--- tribes_alerts/discord4.pl
+++ tribes_alerts/discord4.pl
@@ -1,8 +1,20 @@
 use common::sense;
 use WebService::Discord::Webhook;
 
-my $url = "https://discord.com/api/webhooks/********";
-my $webhook = WebService::Discord::Webhook->new( $url );
+my $url = "https://discord.com/api/webhooks/*****";
+my $webhook = WebService::Discord::Webhook->new( url => $url, wait => 1);
 $webhook->get();
 print "Webhook posting as '" . $webhook->{name} ."' in channel " . $webhook->{channel_id} . "\n";
-$webhook->execute( content => qq{$ARGV[0]\n$ARGV[1]\n$ARGV[2]\n$ARGV[3]\n$ARGV[4]} );
+open(LAST,'discord4.txt');
+my $id = <LAST>;
+close LAST;
+my $curl = 'curl -X DELETE '.$url .'/messages/'.$id;
+`$curl`;
+my $return = $webhook->execute( content => qq{$ARGV[0]\n$ARGV[1]\n$ARGV[2]\n$ARGV[3]\n$ARGV[4]} );
+open(LAST,'>discord4.txt');
+print LAST $return->{id};
+close LAST;
+
+
+
+
